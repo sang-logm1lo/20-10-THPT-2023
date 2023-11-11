ab001ab792727e2c61bc73db53c5e0b5220f1251
idkhow2type <lecaominhhn@gmail.com>
2023-11-11 17:35:53 +0700

    open animation

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const target = document.querySelector(".target");
 
 target.addEventListener("click", () => {
-    alert("Target clicked");
+    const ribbon = document.querySelector(".ribbon");
+    const top = document.querySelector(".top");
+
+    ribbon.classList.add('open')
+    ribbon.addEventListener("transitionend", () => {
+        top.classList.add('open')
+    })
 })
