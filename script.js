2ec81fd3707e0c0088fd79ac68332dec2e246ea9
idkhow2type <lecaominhhn@gmail.com>
2023-11-11 22:13:43 +0700

    Update title and add favicon, animate button on click

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ target.addEventListener("click", () => {
     const ribbon = document.querySelector(".ribbon");
     const top = document.querySelector(".top");
 
+    target.classList.add('open')
     ribbon.classList.add('open')
     ribbon.addEventListener("transitionend", () => {
         top.classList.add('open')
